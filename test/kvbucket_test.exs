855c3ad548628aa098f5786adb8e43d0d6e0b1ed
xdNuggets <joshpatg2005@gmail.com>
2026-05-22 12:55:26 +0100

    test: add setup to clean working directory before tests run to ensure tests are clean

--- test/kvbucket_test.exs
+++ test/kvbucket_test.exs
@@ -3,6 +3,15 @@ defmodule KvBucketTest do
   doctest KvBucket
   import KvBucket
 
+  setup_all do
+    clean_working_dir()
+  end
+
+  defp clean_working_dir() do
+    File.rm_rf!("anubis")
+    :ok
+  end
+
   test "starts a kvb" do
     assert start() == :ok
   end
