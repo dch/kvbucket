d91de2728384c2bcd116595be4054761946f1323
Josh <66138246+JoshCantCode@users.noreply.github.com>
2026-05-25 14:04:21 +0100

    Concurrency (#1)
    * test: add setup to clean working directory before tests run to ensure tests are clean
    
    * test: concurrent test (wip)
    
    * test: Move start/stop into setup / tear down phases
    
    This allows the main body of tests to always have a running KvBucket
    instance to work with, and still ensures that start/stop work as
    expected by assertion in their respective phases.
    
    * kvbucket:Add more doctests in advance of implementation
    
    * kvbucket: add get/2 and put/2 implemention and rework start/0
    
    Fixed doctests not running in the correct order
    Reworked start implementation to return existing khepri instance if it is already running
    Added docs for get/1 and get/2
    Added put implementation
    
    ---------
    
    Co-authored-by: Dave Cottlehuber <dch@skunkwerks.at>

--- test/kvbucket_test.exs
+++ test/kvbucket_test.exs
@@ -3,11 +3,23 @@ defmodule KvBucketTest do
   doctest KvBucket
   import KvBucket
 
-  test "starts a kvb" do
-    assert start() == :ok
+  setup_all do
+    on_exit(fn -> tear_down() end)
+    :ok == clean_working_dir()
+    start()
   end
 
-  test "stops a kvb" do
-    assert stop() == :ok
+  defp clean_working_dir() do
+    File.rm_rf!("anubis")
+    :ok
+  end
+
+  defp tear_down do
+    :ok == stop()
+    clean_working_dir()
+  end
+
+  test "get/1 returns not found if value doesn't exist" do
+    assert get("test") == {:error, :not_found}
   end
 end
